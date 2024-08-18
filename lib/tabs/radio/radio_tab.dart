cd1a1bc13745343b3e744942fd8d789d1119134d
Ehab Ahmed <ko840482@gmail.com>
2024-08-18 15:35:48 +0300

    Implement radio feature

--- lib/tabs/radio/radio_tab.dart
+++ lib/tabs/radio/radio_tab.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:islami/app_theme.dart';
 
-class RadioTab extends StatelessWidget {
+class RadioTab extends StatefulWidget {
+  @override
+  State<RadioTab> createState() => _RadioTabState();
+}
+
+class _RadioTabState extends State<RadioTab> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrange,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/radio_image.png'),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          'اذاعة القرأن الكريم',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.skip_previous_sharp,
+              size: 40,
+              color: AppTheme.lightPrimary,
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            Icon(
+              Icons.play_arrow,
+              size: 40,
+              color: AppTheme.lightPrimary,
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            Icon(
+              Icons.skip_next_sharp,
+              size: 40,
+              color: AppTheme.lightPrimary,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
